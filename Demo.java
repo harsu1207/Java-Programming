db4b5a71e73ecd9c577c9b9b6a98a6f9bd7b447b
Harshit Tomar <ht12072007@gmail.com>
2026-06-15 21:23:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+
+    public static void main() {
+
+        System.out.println("Hello World");
+
+
+        // sum of two number
+        int a = 15  , b = 10;
+        int sum = a+b;
+        System.out.println("sum of " + a + " and " + b + " is = " + sum);
+
+    }
+}
+
+
---
+++ Types.java
@@ -0,0 +1,32 @@
+public class Types {
+
+    static void main() {
+
+        // 1 byte == 8 bite
+
+        // Primitive Data Types
+
+        /*
+        1. byte == 1 bytes == stores whole number from -128 to 127
+        2. short == 2 bytes == stores whole numbers from -32768 to 32767
+        3. int == 4 bytes == stores whole numbers from -2147483648 to 214748367
+        4. float == 4 bytes == stores fractional numbers upto 7 decimal digit
+        5. double == 8 bytes == stores fractional numbers upto 15 decimal digit
+        6. boolean == 1 bytes == stores true or false
+        7. char == 2 bytes == stores a single character value
+         */
+
+
+         byte b = 100;
+         // by default all numeric value is in int form
+         short s = 200;
+         int i = 5868548;
+         float f = 1.5f;
+         // all floating numbers are in double
+        double d = 90.54;
+        char c = 'a';
+
+
+
+    }
+}
